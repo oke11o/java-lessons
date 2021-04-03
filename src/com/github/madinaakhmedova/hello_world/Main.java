eac74657b7d2494072bb4c574bcc75c7dc4be132
madinaakhmedova <akhmedovamadina11@gmail.com>
2021-04-03 22:33:13 +0300

    Exercise 2 and Exercise 3

--- src/com/github/madinaakhmedova/hello_world/Main.java
+++ src/com/github/madinaakhmedova/hello_world/Main.java
@@ -2,6 +2,18 @@ package com.github.madinaakhmedova.hello_world;
 
 public class Main {
     public static void main(String[] args) {
+        // Exercise 1
         System.out.println("Hello world!!!");
+
+        // Exercise 2
+        double var1 = 337.32;
+        byte var2 = -128;
+
+        // Exercise 3
+        int[] ar = {1, 2, 3, 4, 5};
+        int v = ar[0];
+        ar[0] = ar[4];
+        ar[4] = v;
+        System.out.println(ar[0] + ar[4]);
     }
 }
