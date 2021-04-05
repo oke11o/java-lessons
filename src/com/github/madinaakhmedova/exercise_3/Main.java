0e37c67849733e758af24ee888f42e2e7a602ca1
madinaakhmedova <akhmedovamadina11@gmail.com>
2021-04-05 20:58:35 +0300

    Fix Exercise 3

--- src/com/github/madinaakhmedova/exercise_3/Main.java
+++ src/com/github/madinaakhmedova/exercise_3/Main.java
@@ -4,9 +4,13 @@ public class Main {
     public static void main(String[] args) {
         // Exercise 3
         int[] ar = {1, 2, 3, 4, 5};
-        int v = ar[0];
-        ar[0] = ar[4];
-        ar[4] = v;
-        System.out.println(ar[0] + ar[2]);
+
+        int firstVal = ar[0];
+        int lastIdx = ar.length - 1;
+        ar[0] = ar[lastIdx];
+        ar[lastIdx] = firstVal;
+
+        int middleIdx = lastIdx/2;
+        System.out.println(ar[0] + ar[middleIdx]);
     }
 }
