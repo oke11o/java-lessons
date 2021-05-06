f92f14d313c8057f2c1edc300effb392d3caf1c2
Sergey Bevzenko <sabevzenko@avito.ru>
2021-05-06 17:59:20 +0300

    Final 1. Currency calc

---
+++ src/com/github/madinaakhmedova/final_1/Main.java
@@ -0,0 +1,16 @@
+package com.github.madinaakhmedova.final_1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Курс доллара (например 67,55):");
+        Double rate = scanner.nextDouble();
+
+        System.out.println("Количество рублей (можно не целое):");
+        Double count = scanner.nextDouble();
+
+        System.out.printf("Итого: %.2f", rate * count);
+    }
+}
